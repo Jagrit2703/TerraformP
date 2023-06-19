41f27a054d15c951c32e7c5f4f91a57caa569ff0
Jagrit Pant <88482372+Jagrit2703@users.noreply.github.com>
2023-06-19 13:47:41 +0530

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import webapp2
 class Mainpage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 app=webapp2.WSGIApplication([('/',Mainpage),],debug=True)
